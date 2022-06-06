d260f8139816574062665b6260c1205f8d790b41
Martin Evans <martindevans@gmail.com>
2022-06-06 16:25:22 +0100

    - Fixed fish comms network editor  - Created demo scenes  - Defined `GlobalChat` button in input manager

--- Assets/Dissonance/Integrations/FishNet/Editor/DissonanceFishNetCommsEditor.cs
+++ Assets/Dissonance/Integrations/FishNet/Editor/DissonanceFishNetCommsEditor.cs
@@ -1,7 +1,9 @@
 using Dissonance.Editor;
+using UnityEditor;
 
 namespace Dissonance.Integrations.FishNet.Editor
 {
     // Editor script for Dissonance Voice Comms
+    [CustomEditor(typeof(DissonanceFishNetComms))]
     public class DissonanceFishNetCommsEditor : BaseDissonnanceCommsNetworkEditor<DissonanceFishNetComms, DissonanceFishNetServer, DissonanceFishNetClient, DissonanceFishNetConnection, Unit, Unit> { }
 }
